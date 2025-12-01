2b82f16f981dc9101ea319eecdf00d506d119914
admin <admin@MacBook-Pro-cua-admin-2.local>
2025-12-01 17:51:33 +0700

    chore(homepage): add policy/gis/logs/data pages

---
+++ data/ethnicities.js
@@ -0,0 +1,8 @@
+﻿window.ETHNICITIES = [
+  "Kinh", "Tày", "Thái", "Hoa", "Khmer", "Mường", "Nùng", "H\'mông", "Dao", "Gia Rai",
+  "Ê Đê", "Ba Na", "Xơ Đăng", "Sán Chay", "Cơ Ho", "Chăm", "Sán Dìu", "Hrê", "Mnông", "Ra Glai",
+  "Xtiêng", "Bru - Vân Kiều", "Thổ", "Giáy", "Cơ Tu", "Gié Triêng", "Mạ", "Khơ Mú", "Co", "Tà Ôi",
+  "Chơ Ro", "Kháng", "Xinh Mun", "Hà Nhì", "Chu Ru", "Lào", "La Chí", "La Ha", "Phù Lá", "La Hủ",
+  "Lự", "Lô Lô", "Chứt", "Mảng", "Pà Thẻn", "Co Lao", "Cống", "Bố Y", "Si La", "Pu Péo",
+  "Brâu", "Ơ Đu", "Rơ Măm"
+];
